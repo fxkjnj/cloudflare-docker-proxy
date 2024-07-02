829943462900e900475918c6bba1fb93e38c252f
srebro.cn <53123002+fxkjnj@users.noreply.github.com>
2024-07-02 19:27:45 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
+  "docker.srebro.site": dockerHub,
+  "quay.srebro.site": "https://quay.io",
+  "gcr.srebro.site": "https://gcr.io",
+  "k8s-gcr.srebro.site": "https://k8s.gcr.io",
+  "k8s.srebro.site": "https://registry.k8s.io",
+  "ghcr.srebro.site": "https://ghcr.io",
+  "cloudsmith.srebro.site": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.lixd.xyz": dockerHub,
+  "docker-staging.srebro.site": dockerHub,
 };
 
 function routeByHosts(host) {
